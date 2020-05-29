45bad342b8f95c797a5d95a8538c6e10ae36003c
sdmn83 <sdmn@mail.ru>
2020-05-29 22:56:47 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,25 +1,19 @@
 var url = 'https://sf-pyw.mosyag.in/sse/vote/'
-
 const cat = document.getElementById('cats')
 const parrot = document.getElementById('parrots')
 const dog = document.getElementById('dogs')
 const res = document.getElementById('res')
-
 function show () {
     res.style.display = '';
 }
-
-
 cat.onclick = function voteCat() {
     fetch(url + "cats", {method: 'post'});
     show ();
         }
-       
 parrot.onclick = function voteParrot() {
     fetch(url + "parrots", {method: 'post'});
     show ();
         }
-       
 dog.onclick = function voteDog() {
     fetch(url + "dogs", {method: 'post'});
     show ();
